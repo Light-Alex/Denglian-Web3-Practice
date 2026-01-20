5d6f2cf1ff2aecdadc4277cbc8de5ce4c8ca2bd9
Light-Alex <245212467@qq.com>
2026-01-20 20:43:48 +0800

    IDO+MiniSwapPool+线性解锁（ Vesting）合约

---
+++ myfirst-foundry/src/TestToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+interface TokenRecipient {
+    function tokensReceived(address _token, uint256 _amount, bytes calldata data) external returns (bool);
+}
+
+contract TestToken is ERC20 { 
+    constructor(string memory name_, string memory symbol_, uint256 initialSupply_) ERC20(name_, symbol_) {
+        _mint(msg.sender, initialSupply_);
+    }
+
+    function transferWithCallback(address _to, uint256 _amount, bytes calldata data) external returns (bool) {
+        _transfer(msg.sender, _to, _amount);
+
+        if (_to.code.length > 0) {
+            require(TokenRecipient(_to).tokensReceived(msg.sender, _amount, data), "transferWithCallback: transfer failed");
+        }
+
+        return true;
+    }
+}
